45c142588fd2d88527cdcf48b2a8d6f078dfc513
dioica <86318428+dioica@users.noreply.github.com>
2023-12-05 10:33:45 +0300

    Update tst_roottest1.cpp

--- tests/tst_roottest1.cpp
+++ tests/tst_roottest1.cpp
@@ -21,11 +21,14 @@ TEST(rootTest, positive_d)
     double x1_answ, x2_answ;
     x1_answ = 1;
     x2_answ = 2;
-
+    
     a = 1;
     b = -3;
     c = 2;
     rootSearch(a, b, c, &x1, &x2);
+
+    int count_roots = 2;
+    ASSERT_EQ(rootSearch(a, b, c, &x1, &x2), count_roots);
     
     ASSERT_EQ(x1, x1_answ);
     ASSERT_EQ(x2, x2_answ);
@@ -44,6 +47,9 @@ TEST(rootTest, zero_d)
     c = 1;
     rootSearch(a, b, c, &x1, &x2);
 
+    int count_roots = 1;
+    ASSERT_EQ(rootSearch(a, b, c, &x1, &x2), count_roots);
+
     ASSERT_EQ(x1, x1_answ);
     ASSERT_EQ(x2, x2_answ);
 }
@@ -60,91 +66,24 @@ TEST(rootTest, negative_d)
     c = 2;
     rootSearch(a, b, c, &x1, &x2);
 
+    
     ASSERT_EQ(rootSearch(a, b, c, &x1, &x2), count_roots);
 }
 
 // 4
-TEST(rootTest, zero_c1)
-{
-    double a, b, c, x1, x2;
-    double x1_answ, x2_answ;
-    x1_answ = 0;
-    x2_answ = 5.2;
 
-    a = 5;
-    b = -26;
-    c = 0;
-    rootSearch(a, b, c, &x1, &x2);
-
-    ASSERT_EQ(x1, x1_answ);
-    ASSERT_EQ(x2, x2_answ);
-}
 
 // 5
-TEST(rootTest, zero_c2)
-{
-    double a, b, c, x1, x2;
-    double x1_answ, x2_answ;
-    x1_answ = -16;
-    x2_answ = 0;
-    
-    a = 4;
-    b = 64;
-    c = 0;
-    rootSearch(a, b, c, &x1, &x2);
 
-    ASSERT_EQ(x1, x1_answ);
-    ASSERT_EQ(x2, x2_answ);
-}
 
 // 6
-TEST(rootTest, zero_b1)
-{
-    double a, b, c, x1, x2;
-    double x1_answ, x2_answ;
-    x1_answ = -7;
-    x2_answ = 7;
 
-    a = 1;
-    b = 0;
-    c = -49;
-    rootSearch(a, b, c, &x1, &x2);
-
-    ASSERT_EQ(x1, x1_answ);
-    ASSERT_EQ(x2, x2_answ);
-}
 
 // 7
-TEST(rootTest, zero_b2)
-{
-    double a, b, c, x1, x2;
-    double x1_answ, x2_answ;
 
-    int count_roots = 0;
-    a = 1;
-    b = 0;
-    c = 3;
-    rootSearch(a, b, c, &x1, &x2);
-
-    ASSERT_EQ(rootSearch(a, b, c, &x1, &x2), count_roots);
-}
 
 // 8
-TEST(rootTest, zero_b_c)
-{
-    double a, b, c, x1, x2;
-    double x1_answ, x2_answ;
-    x1_answ = 0;
-    x2_answ = 0;
 
-    a = 5;
-    b = 0;
-    c = 0;
-    rootSearch(a, b, c, &x1, &x2);
-
-    ASSERT_EQ(x1, x1_answ);
-    ASSERT_EQ(x2, x2_answ);
-}
 
 // 9
 TEST(rootTest, zero_a)
@@ -160,125 +99,51 @@ TEST(rootTest, zero_a)
     c = 50;
     rootSearch(a, b, c, &x1, &x2);
 
+    int count_roots = 1;
+    ASSERT_EQ(rootSearch(a, b, c, &x1, &x2), count_roots);
+    
     ASSERT_EQ(x1, x1_answ);
     ASSERT_EQ(x2, x2_answ);
 }
 
-// 10
-TEST(rootTest, zero_a2)
-{
-    double a, b, c, x1, x2;
-    double x1_answ, x2_answ;
-    x1_answ = -5;
-    x2_answ = -5;
 
-    a = 0;
-    b = -10;
-    c = -50;
-    rootSearch(a, b, c, &x1, &x2);
-
-    ASSERT_EQ(x1, x1_answ);
-    ASSERT_EQ(x2, x2_answ);
-}
-
-// 11
-TEST(rootTest, zero_a3)
-{
-    double a, b, c, x1, x2;
-    double x1_answ, x2_answ;
-    x1_answ = -1;
-    x2_answ = 4;
-
-    a = 2;
-    b = -6;
-    c = -8;
-    rootSearch(a, b, c, &x1, &x2);
-
-    ASSERT_EQ(x1, x1_answ);
-    ASSERT_EQ(x2, x2_answ);
-}
 
 // 12
 TEST(rootTest, zero_a_b1)
 {
     double a, b, c, x1, x2;
     double x1_answ, x2_answ;
-    x1_answ = -50;
-    x2_answ = -50;
 
     a = 0;
     b = 0;
     c = 50;
     rootSearch(a, b, c, &x1, &x2);
 
-    ASSERT_EQ(x1, x1_answ);
-    ASSERT_EQ(x2, x2_answ);
+    int count_roots = 0;
+    ASSERT_EQ(rootSearch(a, b, c, &x1, &x2), count_roots);
+
 }
 
 // 13
-TEST(rootTest, zero_a_b2)
-{
-    double a, b, c, x1, x2;
-    double x1_answ, x2_answ;
-    x1_answ = 10;
-    x2_answ = 10;
 
-    a = 0;
-    b = 0;
-    c = -10;
-    rootSearch(a, b, c, &x1, &x2);
-
-    ASSERT_EQ(x1, x1_answ);
-    ASSERT_EQ(x2, x2_answ);
-}
 
 // 14
 TEST(rootTest, zero_a_b_c)
 {
     double a, b, c, x1, x2;
     double x1_answ, x2_answ;
-    x1_answ = 0;
-    x2_answ = 0;
 
     a = 0;
     b = 0;
     c = 0;
     rootSearch(a, b, c, &x1, &x2);
 
-    ASSERT_EQ(x1, x1_answ);
-    ASSERT_EQ(x2, x2_answ);
+    int count_roots = 0;
+    ASSERT_EQ(rootSearch(a, b, c, &x1, &x2), count_roots);
 }
 
 // 15
-TEST(rootTest, zero_a_c1)
-{
-    double a, b, c, x1, x2;
-    double x1_answ, x2_answ;
-    x1_answ = 0;
-    x2_answ = 0;
 
-    a = 0;
-    b = 6;
-    c = 0;
-    rootSearch(a, b, c, &x1, &x2);
-
-    ASSERT_EQ(x1, x1_answ);
-    ASSERT_EQ(x2, x2_answ);
-}
 
 // 16
-TEST(rootTest, zero_a_c2)
-{
-    double a, b, c, x1, x2;
-    double x1_answ, x2_answ;
-    x1_answ = 0;
-    x2_answ = 0;
 
-    a = 0;
-    b = -6;
-    c = 0;
-    rootSearch(a, b, c, &x1, &x2);
-
-    ASSERT_EQ(x1, x1_answ);
-    ASSERT_EQ(x2, x2_answ);
-}
