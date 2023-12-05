59ebfeed86d580581064a8c68013dc5acac01fd0
dioica <86318428+dioica@users.noreply.github.com>
2023-12-05 09:59:22 +0300

    Update myfunc.h

--- app/myfunc.h
+++ app/myfunc.h
@@ -7,7 +7,7 @@ int fibonachi(int num);
 
 void printStdoutMessages();
 
-void rootSearch(double a, double b, double c, double* x1, double* x2);
+int rootSearch(double a, double b, double c, double* x1, double* x2);
 
 extern int val;
 
